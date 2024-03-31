dbc5b64c666a3a98acaff0c47297210bb0c1f728
bijeet1221 <116748820+bijeet1221@users.noreply.github.com>
2024-03-31 15:45:25 +0530

    Delete main directory

--- main/java/com/iiitb/imageEffectApplication/baseEffects/SingleValueDiscreteEffect.java
+++
@@ -1,7 +0,0 @@
-package com.iiitb.imageEffectApplication.baseEffects;
-
-import com.iiitb.imageEffectApplication.exception.IllegalParameterException;
-
-public interface SingleValueDiscreteEffect extends PhotoEffect {
-    void setParameterValue(int parameterValue) throws IllegalParameterException;
-}
